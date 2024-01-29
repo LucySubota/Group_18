381cb5bab1d274b4a931c6123008c4b4fe675244
martahalak <martagalak2103@gmail.com>
2024-01-29 12:18:47 -0600

    Completed

--- src/group_Studies/OddOrEven.java
+++ src/group_Studies/OddOrEven.java
@@ -1,4 +1,16 @@
 package group_Studies;
 
 public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        int number = 50;
+
+        boolean isEven = number % 2 == 0;
+        boolean isOdd = !isEven;
+
+        System.out.println(number+ " is an even number: " +isEven);
+        System.out.println(number+ " is an odd number: " +isOdd);
+
+    }
 }
