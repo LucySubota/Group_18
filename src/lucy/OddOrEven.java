1730d63371d2b7d01ac530f31f1ed15c4617725b
LucySubota <lucysubota@gmail.com>
2024-01-29 17:47:20 -0600

    created lucy package and added one file

---
+++ src/lucy/OddOrEven.java
@@ -0,0 +1,10 @@
+package lucy;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        result(0);
+    }
+    public static void result(int n){
+        System.out.println((n%2 == 0)? "Even" : "Odd");
+    }
+}
