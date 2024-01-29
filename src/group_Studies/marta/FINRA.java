c5a81eaef2129ae2a6187a79107edd66a8c0a3e2
martahalak <martagalak2103@gmail.com>
2024-01-29 14:38:04 -0600

    Completed new class

--- src/group_Studies/marta/FINRA.java
+++ src/group_Studies/marta/FINRA.java
@@ -1,4 +1,28 @@
 package group_Studies.marta;
 
+import java.util.Scanner;
+
 public class FINRA {
+
+    public static void main(String[] args) {
+        finra();
+    }
+    public static void finra (){
+
+        for (int number = 1; number <= 30; number++) {
+
+            if (number%3==0 && number%5==0){
+                System.out.println("FINRA");
+            } else if (number%5==0) {
+                System.out.println("RA");
+            } else if (number%3==0 ) {
+                System.out.println("FIN");
+            }else {
+                System.out.println(number);
+            }
+
+        }
+
+    }
+
 }
