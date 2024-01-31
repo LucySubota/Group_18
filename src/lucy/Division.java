54ae68e1807f6d3bcb918742ad2caef12e23ef6b
LucySubota <lucysubota@gmail.com>
2024-01-31 10:48:41 -0600

    edited division again

--- src/lucy/Division.java
+++ src/lucy/Division.java
@@ -3,16 +3,20 @@ package lucy;
 public class Division {
     public static void main(String[] args) {
 
-        System.out.println(result(3,4));
+        System.out.println(result(3,1));
 
     }
     public static String result (int n1, int n2){
 
-        try {
+       /* try {
          int  n = n1/n2;
         }catch (ArithmeticException e){
             e.printStackTrace();
             return "";
+        }*/
+
+        if(n2 == 0){
+            throw new ArithmeticException("Can not divide by 0");
         }
 
         int result = 0;
