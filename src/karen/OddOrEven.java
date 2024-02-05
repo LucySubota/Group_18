ac3d5439ec287d38ae6c31d411c7c7305a0beb2f
kobam <Kobam78@gmail.com>
2024-02-05 08:37:12 -0600

    Added Odd or Even  Task

--- src/karen/OddOrEven.java
+++ src/karen/OddOrEven.java
@@ -23,12 +23,12 @@ import java.util.Scanner;
             }
         }
     }
-    
+
 
 /* Numbers Odd or even:
 Write  a method which can identify given number is even or odd
 Output ex:
 identify(5); ->"Odd"
-identify(6); ->"Even" */ 
-    
+identify(6); ->"Even" */
+
 
