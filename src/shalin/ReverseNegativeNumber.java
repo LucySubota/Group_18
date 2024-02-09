9838d0cce4a1afcc24de7bdd5a995453bd1c5e0f
shalincydeo <cydeolearning@gmail.com>
2024-02-09 13:03:46 -0600

    updated ReverseNegativeNumber.java file

--- src/shalin/ReverseNegativeNumber.java
+++ src/shalin/ReverseNegativeNumber.java
@@ -10,13 +10,16 @@ public class ReverseNegativeNumber {
         System.out.println(reverseNegative(0));
         System.out.println(reverseNegative(7));
         System.out.println(reverseNegative(15));
+        System.out.println(reverseNegative(-10));
+        System.out.println(reverseNegative(-30));
+        System.out.println(reverseNegative(-50));
 
 
     }
 
     public static int reverseNegative(int num) {
 
-        if (num < 0 && num != -10) {
+        if (num < 0 && num % 10 != 0) {
             int rev = 0;
             while (num != 0) {
                 int lastDigit = num % 10;
