5af0dbe42984583bbf2ba021410ed75ed1b25c0f
hmarta21 <martagalak2103@gmail.com>
2024-02-09 20:07:11 -0600

    Completed ReverseNegativeNumber

--- src/group_Studies/marta/ReverseNegativeNumber.java
+++ src/group_Studies/marta/ReverseNegativeNumber.java
@@ -1,4 +1,27 @@
 package group_Studies.marta;
 
 public class ReverseNegativeNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseNegative(0));
+        System.out.println(reverseNegative(-35));
+        System.out.println(reverseNegative(-10));
+
+    }
+
+    public static int reverseNegative(int num) {
+
+        if (num < 0 ) {
+            int reverse = 0;
+            while (num != 0) {
+                int lastDigit = num % 10;
+                reverse = reverse * 10 + lastDigit;
+                num /= 10;
+            }
+            return reverse;
+        }
+        return num;
+
+    }
 }
