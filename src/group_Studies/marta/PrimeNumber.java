d4d0ba965e5e78c9b7584086295a116441fb16c8
hmarta21 <martagalak2103@gmail.com>
2024-02-09 19:41:47 -0600

    Completed PrimeNumber

--- src/group_Studies/marta/PrimeNumber.java
+++ src/group_Studies/marta/PrimeNumber.java
@@ -1,4 +1,21 @@
 package group_Studies.marta;
 
 public class PrimeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(primeNumber(1));
+    }
+    public static boolean primeNumber(int n){
+
+        if (n < 2){
+            return false;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
