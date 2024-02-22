c0a42dcde47431513f72c00cac1edc85e8de33ff
hmarta21 <martagalak2103@gmail.com>
2024-02-21 20:03:51 -0600

    Created class

---
+++ src/group_Studies/marta/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+package group_Studies.marta;
+
+public class RemoveDuplicates {
+
+
+
+}
+/*
+Write a return method that can remove the duplicated values from the String
+  Ex: removeDup("AAABBBCCC") ==> ABC
+ */
