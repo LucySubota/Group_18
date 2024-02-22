07f5533ddd771df7079bb6b620d466b82857a44e
hmarta21 <martagalak2103@gmail.com>
2024-02-21 20:13:17 -0600

    Completed class

--- src/group_Studies/marta/RemoveDuplicates.java
+++ src/group_Studies/marta/RemoveDuplicates.java
@@ -2,6 +2,28 @@ package group_Studies.marta;
 
 public class RemoveDuplicates {
 
+    public static void main(String[] args) {
+
+        System.out.println(removeDuplicates("AAABBBCCC"));
+    }
+
+    public static String removeDuplicates(String str){
+
+        String result = "";
+
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if( result.contains( ""+str.charAt(i) )){ // if the character is already contained in new string
+                continue; // skip it
+            }
+
+            result += str.charAt(i);
+
+        }
+
+        return result;
+    }
 
 
 }
