fbfefa45eebf554341f8f9ca0ac4cd55c8442153
hmarta21 <martagalak2103@gmail.com>
2024-02-21 19:53:50 -0600

    Created class

---
+++ src/group_Studies/marta/SameLetters.java
@@ -0,0 +1,40 @@
+package group_Studies.marta;
+
+import java.util.Arrays;
+
+public class SameLetters {
+
+    public static void main(String[] args) {
+
+//        String str1 = "abc";
+//        String str2 = "cab";
+//
+//        String str3 = "abc";
+//        String str4 = "abb";
+//
+//        char[] a1 = str1.toCharArray();
+//        char[] a2 = str2.toCharArray();
+//
+//        char[] a3 = str3.toCharArray();
+//        char[] a4 = str4.toCharArray();
+//
+//        Arrays.sort(a1);
+//        Arrays.sort(a2);
+//
+//        Arrays.sort(a3);
+//        Arrays.sort(a4);
+//
+//        boolean isSameLetters = Arrays.equals(a1, a2);
+//        boolean sameLetters = Arrays.equals(a3, a4);
+//
+//        System.out.println("Is same letters: " + isSameLetters);
+//        System.out.println("Same letters: " + sameLetters);
+
+    }
+
+}
+/*
+Write a return method that checks if a string is built out of the same letters as another string.
+  Ex: same("abc", "cab"); ==> true
+      same("abc", "abb"); ==> false
+ */
