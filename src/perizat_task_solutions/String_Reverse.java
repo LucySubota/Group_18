b1ba026c3da0388a9f73644716306d323a153816
perizat5 <aperizat45@gmail.com>
2024-03-05 17:12:07 -0600

    added Task solution for "Divisible by "3, 5, 15"

---
+++ src/perizat_task_solutions/String_Reverse.java
@@ -0,0 +1,19 @@
+package perizat_task_solutions;
+
+public class String_Reverse {
+    public static void main(String[] args) {
+        System.out.println(reverse_method("Cydeo"));
+    }
+
+    public static String reverse_method(String str){
+        String str2="";
+        for(int i=str.length()-1; i>=0; i--){
+            str2 = str2+str.charAt(i);
+        }
+        return str2;
+    }
+}
+/*
+Write a return method that can reverse  String
+Ex: Reverse("ABCD"); ==> DCBA
+ */
