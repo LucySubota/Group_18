129274f9ab057c197082199e3118185546e07cd2
hmarta21 <martagalak2103@gmail.com>
2024-03-09 13:00:00 -0600

    Completed Array_FindMaximum

--- src/group_Studies/marta/Array_FindMaximum.java
+++ src/group_Studies/marta/Array_FindMaximum.java
@@ -1,4 +1,29 @@
 package group_Studies.marta;
 
 public class Array_FindMaximum {
+
+    public static void main(String[] args) {
+
+        System.out.println(findMaximum(new int[] {99, 12, 23, 32, 44, 57, 6}));
+    }
+
+    public static int findMaximum(int[] arr){
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max){
+
+                max = arr[1];
+            }
+        }
+
+        return max;
+    }
+
 }
+/*
+Write a method that can find the maximum number from an int Array
+Ex:  int[] arr = {99, 12, 23, 32, 44, 57, 6} ==> 99
+ */
