743a949afed2e9fa97ef8a98fb7ed3e75e637986
hmarta21 <martagalak2103@gmail.com>
2024-03-14 17:53:59 -0500

    Made changes

--- src/group_Studies/marta/Array_FindMaximum.java
+++ src/group_Studies/marta/Array_FindMaximum.java
@@ -5,17 +5,18 @@ public class Array_FindMaximum {
     public static void main(String[] args) {
 
         System.out.println(findMaximum(new int[] {99, 12, 23, 32, 44, 57, 6}));
+        System.out.println(findMaximum(new int[] {9, 12, 23, 32, 44, 57, 6}));
     }
 
     public static int findMaximum(int[] arr){
 
         int max = arr[0];
 
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
 
             if (arr[i] > max){
 
-                max = arr[1];
+                max = arr[i];
             }
         }
 
